eb27f87a21b5326a98df19428d4c84cf08736f57
HouYi9380 <328419036@qq.com>
2017-11-08 22:45:57 +0800

    第一次提交数据

---
+++ src/cn/janlinker/testGit/helloGit.java
@@ -0,0 +1,10 @@
+package cn.janlinker.testGit;
+
+/**
+ * Created by WY on 2017/11/8.
+ */
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
