1f20c88356a9e456b4b6d15c411a0936bb46c2d8
e <328419036@qq.com>
2017-11-09 11:07:00 +0800

    win10上增加了一行代码

--- src/cn/janlinker/testGit/helloGit.java
+++ src/cn/janlinker/testGit/helloGit.java
@@ -7,5 +7,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("i can play in win10");
+        System.out.println("i create a new branch win10");
     }
 }
